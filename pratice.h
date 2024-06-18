ca187404723f9e1fda0cb8847e9521a3028d5561
boredfifo <153274843+boredfifo@users.noreply.github.com>
2024-06-18 17:49:35 +0200

    Update pratice.h
    added a new function check_flag to create and update a flag if one of the IR sensors go off

--- pratice.h
+++ pratice.h
@@ -6,11 +6,14 @@ class Pratice{
    public: 
          void run_left();
          void run_right();
+         int check_flag();
          void stop();
          void gpio_init();
          void move_left();
          void move_right();
-         void movement();
+         void evade();
+         void park();
+         void turn();
          static const int IR_PIN_1 = 12;
          static const int IR_PIN_2 = 8;
 };
