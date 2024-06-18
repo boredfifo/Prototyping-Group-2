a7ef7d0be594238a1a16aa593c8fa501b06983b8
moizZM <153621024+moizZM@users.noreply.github.com>
2024-06-18 17:42:40 +0200

    Add files via upload

--- ultrasonic.cpp
+++ ultrasonic.cpp
@@ -1,44 +1,29 @@
 #include "ultrasonic.h"
 #include "Arduino.h"
-#include "Servo.h"
 
-Servo servo1; 
 
 void Ultrasonic::init(){
-	//servo1.attach(SERVO_PIN);
   pinMode(ECHO_PIN, INPUT);
   pinMode(TRIG_PIN, OUTPUT);
 }
 
-void Ultrasonic::move_left(){
-  int Position = 0;
-	for(Position = 0; Position <= 90; Position ++){
-		servo1.write(Position);
-		delay(10);
-	}
-  for(Position = 90; Position >= 0; Position --){
-		servo1.write(Position);
-		delay(10);
-	}
-}
-void Ultrasonic::move_right(){
-  int Position1 = 90;
-	for(Position1 = 90; Position1 <= 180; Position1 ++){
-		servo1.write(Position1);
-		delay(10);
-	}
-  for(Position1 = 180; Position1 >= 90; Position1 --){
-		servo1.write(Position1);
-		delay(10);
-	}
-}
 
-
+void Ultrasonic::low(){
+  digitalWrite(TRIG_PIN, LOW);
+  }
 
 void Ultrasonic::trigger(){
   digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(5);
+  delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(50);
+  delayMicroseconds(30);
   digitalWrite(TRIG_PIN, LOW);
 }
+
+int Ultrasonic::distance(){
+    int counter = 0;
+    int distance = 0;
+    counter = pulseIn(ECHO_PIN, HIGH);
+    distance = counter * 0.017;
+    return distance;
+  }
